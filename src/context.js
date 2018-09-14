6570be111bfed6c2d31601a3611d948b7ab029fa
Oliver Wach <git@abramov-software.de>
2018-09-14 17:17:16 +0200

    Lifecycle Methods and Get Request to JSON Placeholder

--- src/context.js
+++ src/context.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 const Context = React.createContext();
 
@@ -23,28 +24,15 @@ const reducer = (state, action) => {
 
 export class Provider extends Component {
   state = {
-    contacts: [
-      {
-        id: 1,
-        name: "John Doe",
-        email: "jdoe@gmail.com",
-        phone: "555-555-5555"
-      },
-      {
-        id: 2,
-        name: "Karen Williams",
-        email: "karen@gmail.com",
-        phone: "333-333-3333"
-      },
-      {
-        id: 3,
-        name: "John Snow",
-        email: "snow@gmail.com",
-        phone: "111-111-1111"
-      }
-    ],
+    contacts: [],
     dispatch: action => this.setState(state => reducer(state, action))
   };
+
+  componentDidMount() {
+    axios
+      .get("http://jsonplaceholder.typicode.com/users")
+      .then(res => this.setState({ contacts: res.data }));
+  }
 
   render() {
     return (
